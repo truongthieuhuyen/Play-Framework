8e5f6ee1fac55533f456e2d5ae23208382a73fe2
truongthieuhuyen <nguyentientruong97@gmail.com>
2021-08-10 20:07:45 +0700

    Model with several functional

--- app/controllers/TaskList.scala
+++ app/controllers/TaskList.scala
@@ -1,30 +1,54 @@
 package controllers
 
 import akka.http.scaladsl.model.HttpHeader.ParsingResult.Ok
+import models.UserInMemory
 import play.api.mvc.{BaseController, ControllerComponents}
 
 import javax.inject.{Inject, Singleton}
 
 @Singleton
 class TaskList @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
-  def taskList = Action {
-    val tasks = List("wake up", "breakfast", "study", "eat lunch", "study", "resting-time", "sleep")
-
-    Ok(views.html.taskPage(tasks))
-  }
 
   def login = Action {
     Ok(views.html.login())
   }
 
+  def register = Action{
+    Ok(views.html.register())
+  }
+
   def validateLogin = Action { request =>
     val postVals = request.body.asFormUrlEncoded
-    postVals.map{args =>
+    postVals.map { args =>
       val username = args("Username").head
       val password = args("Password").head
-
-      Redirect(routes.TaskList.taskList)
+      if (UserInMemory.validateUser(username, password)) {
+        Redirect(routes.TaskList.taskList)
+      }
+      else {
+        Redirect(routes.TaskList.login)
+      }
     }.getOrElse(Redirect(routes.TaskList.login))
   }
 
+  def createUser = Action{ request =>
+    val postVals = request.body.asFormUrlEncoded
+    postVals.map { args =>
+      val username = args("Username").head
+      val password = args("Password").head
+      if (UserInMemory.createUser(username, password)) {
+        Redirect(routes.TaskList.taskList)
+      }
+      else {
+        Redirect(routes.TaskList.register)
+      }
+    }.getOrElse(Redirect(routes.TaskList.register))
+  }
+
+  def taskList = Action {
+    val username = "mrkable"
+    val tasks = UserInMemory.getTasks(username)
+
+    Ok(views.html.taskPage(tasks))
+  }
 }
---
+++ app/models/UserInMemory.scala
@@ -0,0 +1,30 @@
+package models
+
+import scala.collection.mutable
+
+
+object UserInMemory {
+  private val users = mutable.Map[String, String]("mrkable" -> "password")
+  private val tasks = mutable.Map[String, List[String]]("mrkable" -> List("receive", "unbox", "shot"))
+
+  def validateUser(username: String, password: String): Boolean = {
+    users.get(username).map(_ == password).getOrElse(false)
+  }
+
+  def createUser(username: String, password: String): Boolean = {
+
+    if (users.contains(username)) false
+    else {
+      users(username) = password
+      true
+    }
+  }
+
+  def getTasks(username: String): Seq[String] = {
+    tasks.get(username).getOrElse(Nil)
+  }
+
+  def addTask(username: String, task: String): Unit = ???
+
+  def removeTask(username: String, index: Int): Boolean = ???
+}
