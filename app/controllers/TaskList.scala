dca2c12ef84947f2318c6c212588490116683f35
truongthieuhuyen <nguyentientruong97@gmail.com>
2021-08-12 19:48:32 +0700

    add & delete task function

--- app/controllers/TaskList.scala
+++ app/controllers/TaskList.scala
@@ -2,15 +2,23 @@ package controllers
 
 import akka.http.scaladsl.model.HttpHeader.ParsingResult.Ok
 import models.UserInMemory
-import play.api.mvc.{BaseController, ControllerComponents}
+import play.api.mvc.{BaseController, ControllerComponents, MessagesAbstractController, MessagesControllerComponents}
+import play.api.data._
+import play.api.data.Forms._
 
 import javax.inject.{Inject, Singleton}
 
+case class LoginData(username: String, password: String)
+
 @Singleton
-class TaskList @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class TaskList @Inject()(val cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
+  val loginForm = Form(mapping(
+    "Username" -> text(3,10),
+    "Password" -> text(4)
+  )(LoginData.apply)(LoginData.unapply))
 
   def login = Action { implicit request =>
-    Ok(views.html.login())
+    Ok(views.html.login(loginForm))
   }
 
   def register = Action { implicit request =>
@@ -33,6 +41,19 @@ class TaskList @Inject()(val controllerComponents: ControllerComponents) extends
         Redirect(routes.TaskList.login).flashing("error" -> "incorrect username/password ")
       }
     }.getOrElse(Redirect(routes.TaskList.login))
+  }
+
+  def validateLoginForm = Action { implicit request =>
+    loginForm.bindFromRequest().fold(
+      formWithErrors => BadRequest(views.html.login(formWithErrors)),
+      ld =>
+        if (UserInMemory.validateUser(ld.username, ld.password)) {
+          Redirect(routes.TaskList.taskList).withSession("username" -> ld.username)
+        }
+        else {
+          Redirect(routes.TaskList.login).flashing("error" -> "incorrect username/password ")
+        }
+    )
   }
 
   def createUser = Action { implicit request =>
