3e18ef1395beb2ea0cf429a096058ee024f93522
truongthieuhuyen <nguyentientruong97@gmail.com>
2021-08-13 19:53:26 +0700

    automated test using selenium to test logging in and check taskList

---
+++ test/controllers/TaskListSpec.scala
@@ -0,0 +1,30 @@
+package controllers
+
+import org.scalatestplus.play.{HtmlUnitFactory, OneBrowserPerSuite, PlaySpec}
+import org.scalatestplus.play.guice.GuiceOneServerPerSuite
+
+class TaskListSpec extends PlaySpec with GuiceOneServerPerSuite with OneBrowserPerSuite with HtmlUnitFactory {
+  "Task list" must {
+    "login and access functions " in {
+      go to s"http://locahost:$port/login"
+//      pageTitle mustBe "Login"
+      find(cssSelector("h2")).isEmpty mustBe false
+      find(cssSelector("h2")).foreach(e => e.text mustBe ("Login"))
+
+      click on "username-login"
+      textField(id("username-login")).value = "mrkable"
+      click on "password-login"
+      pwdField(id("password-login")).value = "pass"
+      submit()
+
+      eventually {
+        println("In eventually")
+        pageTitle mustBe("Task List")
+        println("Title passed")
+        find(cssSelector("h1")).isEmpty mustBe(false)
+        println("found h2")
+        find(cssSelector("h1")).foreach(e => e.text mustBe "Task")
+      }
+    }
+  }
+}
