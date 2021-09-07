538b3cd4525c46fe574cba041b82a4a1b22d21c7
truongthieuhuyen <nguyentientruong97@gmail.com>
2021-09-08 00:07:20 +0700

    modified createUser method

--- app/models/TaskDatabaseModel.scala
+++ app/models/TaskDatabaseModel.scala
@@ -8,12 +8,12 @@ import org.mindrot.jbcrypt.BCrypt
 
 class TaskDatabaseModel (db: Database)(implicit ec: ExecutionContext){
   def validateUser(username: String, password: String): Future[Boolean] = {
-    val matches = db.run(Users.filter(UsersRow => UsersRow.username === username && UsersRow.password === password).result)
-    matches.map(UsersRow => UsersRow.nonEmpty)
+    val matches = db.run(Users.filter(UsersRow => UsersRow.username === username ).result)
+    matches.map(UsersRow => UsersRow.filter(UsersRow => BCrypt.checkpw(password, UsersRow.password)).nonEmpty)
   }
 
-  def createUser(username: String, password: String): Boolean = {
-    ???
+  def createUser(username: String, password: String): Future[Boolean] = {
+    db.run(Users += UsersRow(-1, username, BCrypt.hashpw(password, BCrypt.gensalt()))).map(addCount => addCount > 0)
   }
 
   def getTasks(username: String): Future[Seq[String]] = {
