c8192175759b8a3839b20b9036dbea6813041535
truongthieuhuyen <nguyentientruong97@gmail.com>
2021-09-07 17:24:53 +0700

    fixed version 3

--- public/javascripts/v3.js
+++ public/javascripts/v3.js
@@ -2,45 +2,17 @@
 
 const validateRoute  = document.getElementById("validateRoute").value;
 const createRoute = document.getElementById("createRoute").value;
-const taskRoute = document.getElementById("taskRoute").value;
+const tasksRoute = document.getElementById("tasksRoute").value;
 const deleteRoute = document.getElementById("deleteRoute").value;
 const addRoute = document.getElementById("addRoute").value;
 const csrfToken= document.getElementById("csrfToken").value;
 const logoutRoute = document.getElementById("logoutRoute").value;
 
 
-function loadTask(){
-    const ul = document.getElementById("task-list");
-    ul.innerHTML = "";
-
-    fetch(taskRoute).then(res => res.json()).then(tasks =>{
-        for(let i = 0; i< task.length; i++){
-            const li = document.getElementById("li");
-            const text = document.getElementById(task[i]);
-            li.appendChild(text);
-            li.onclick = e => {
-                fetch(deleteRoute, {
-                    method:'POST',
-                    headers:{'Content-Type': 'application/json', 'Csrf-Token': csrfToken},
-                    body: JSON.stringify(i)
-                }).then(res => res.json()).then(data => {
-                    if(data){
-                        document.getElementById("task-message").innerHTML = "";
-                        loadTask();
-                    }else{
-                        document.getElementById("task-message").innerHTML = "Failed to delete";
-                    }
-                });
-            }
-            ul.appendChild(i);
-        }
-    });
-}
-
 function login(){
     console.log("starting login");
-    const username = document.getElementById("#loginName").value;
-    const password = document.getElementById("#loginPass").value;
+    const username = document.getElementById("loginName").value;
+    const password = document.getElementById("loginPass").value;
     fetch(validateRoute, {
         method: 'POST',
         headers: {'Content-Type': 'application/json', 'Csrf-Token': csrfToken},
@@ -52,7 +24,7 @@ function login(){
             document.getElementById("task-section").hidden = false
             document.getElementById("login-message").innerHTML = "";
             document.getElementById("create-message").innerHTML = "";
-            loadTask();
+            loadTasks();
         }else {
             document.getElementById("login-message").innerHTML = "Login failed";
         }
