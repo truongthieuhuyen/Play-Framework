4288587e5b3507dbe6dd633cf0e8253d493291a6
truongthieuhuyen <nguyentientruong97@gmail.com>
2021-09-07 11:39:26 +0700

    fixed driver

--- app/models/CodeGen.scala
+++ app/models/CodeGen.scala
@@ -3,9 +3,9 @@ package models
 object CodeGen extends App {
   slick.codegen.SourceCodeGenerator.run(
     "slick.jdbc.MySQLProfile",
-    "org.mysql.Driver",
+    "com.mysql.jdbc.Driver",
     "jdbc:mysql://localhost/task_list?user=root&password=duongmich",
-//    "D:\\myDocuments\\Scala\\intern\\play-framework-starter-package\\app",
+    "D:\\myDocuments\\Scala\\intern\\play-framework-starter-package\\app",
     "models", None, None, true, false
   )
 }
